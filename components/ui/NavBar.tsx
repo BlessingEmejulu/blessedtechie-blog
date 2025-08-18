b8977d4494499fbe09ea42260a38a7c4f0f5ca8c
BlessingEmejulu <blessingnneamaka57@gmail.com>
2025-08-18 04:27:30 +0100

    Add NavBar Component

---
+++ components/ui/NavBar.tsx
@@ -0,0 +1,59 @@
+// components/NavBar.tsx
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+import { Sun, Moon } from "lucide-react"; 
+import { Geist_Mono as GeistMonoFont } from "next/font/google";
+
+const geistMono = GeistMonoFont({ subsets: ["latin"] });
+
+export default function NavBar() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  return (
+    <nav
+      className="border-t-4 border-transparent bg-white
+                 bg-gradient-to-r from-[#8E63C8] via-[#4BAE77] to-[#FFD765]
+                 bg-[length:100%_4px] bg-top bg-no-repeat 
+                 flex items-center justify-between px-6 py-3"
+    >
+      {/* Logo */}
+      <div className="flex items-center">
+        <Image
+          src="/blessingemejulu.png"
+          alt="Logo"
+          width={40}
+          height={40}
+          className="rounded"
+        />
+      </div>
+
+      {/* Links + Toggle */}
+    <div className={`${geistMono.className} flex items-center gap-6`}>
+        <Link href="/articles" className="text-purple-900 hover:underline">
+          Articles
+        </Link>
+          <Link href="/projects" className="text-purple-900 hover:underline">
+        Projects
+        </Link>
+        <Link href="/contact" className="text-purple-900 hover:underline">
+          Contact
+        </Link>
+
+        {/* Toggle Switch */}
+        <button
+          onClick={() => setDarkMode(!darkMode)}
+          className="w-8 h-8 flex items-center rounded-full px-1 transition-all duration-300 bg-purple-800"
+        >
+          {darkMode ? (
+            <Moon className="text-yellow-300 w-6 h-6" />
+          ) : (
+            <Sun className="text-yellow-400 w-6 h-6" />
+          )}
+        </button>
+      </div>
+    </nav>
+  );
+}
