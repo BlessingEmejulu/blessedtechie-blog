f6ddfffc79d5771a44b9795ea4a6817bfae1c98b
BlessingEmejulu <blessingnneamaka57@gmail.com>
2025-08-27 00:23:02 +0100

    feat: restructure Sanity schemas and reintroduce ArticlesPage component

---
+++ sanity/index.ts
@@ -0,0 +1,9 @@
+// sanity/schema.ts
+import { type SchemaTypeDefinition } from 'sanity';
+import post from './schemas/post';
+import author from './schemas/author';
+import category from './schemas/category';
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [post, author, category],
+};
